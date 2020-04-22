aa1fb1006e6fb6b550e6eaaa130022e3e3c12d02
Umar <uhassan15@gmail.com>
2020-04-22 03:36:52 -0500

    re-order

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,9 @@
-// @TODO: YOUR CODE HERE!
+//setting parameters 
+
+let svgHeight = 450;
+let svgWidth = 900;
+let margin = { top: 15, right: 30, bottom: 60, left: 80};
+let height = svgHeight - margin.left - margin.right;
+let width = svgWidth - margin.top - margin.bottom;
+
+
